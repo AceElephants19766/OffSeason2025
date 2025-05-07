8af4f37cfb49b16a55f5118462ed2f036e9a5b84
AceElephants <elephants19766@gmail.com>
2025-05-07 20:49:58 +0300

    servo!

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test.java
@@ -2,11 +2,12 @@ package org.firstinspires.ftc.teamcode;
 
 import com.arcrobotics.ftclib.command.CommandOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp
 public class Test extends CommandOpMode {
     @Override
     public void initialize() {
-
+        hardwareMap.get(Servo.class, "servo").setPosition(0.5);
     }
 }

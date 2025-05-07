83a9cddf7823f90c75f6fe8c5a5ece236322e02b
AceElephants <elephants19766@gmail.com>
2025-05-07 20:17:36 +0300

    added ftclib
    ibhbihfgwigr2biugu2h9h2gi

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test.java
@@ -1,2 +1,12 @@
-package org.firstinspires.ftc.teamcode;public class Test {
+package org.firstinspires.ftc.teamcode;
+
+import com.arcrobotics.ftclib.command.CommandOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp
+public class Test extends CommandOpMode {
+    @Override
+    public void initialize() {
+
+    }
 }

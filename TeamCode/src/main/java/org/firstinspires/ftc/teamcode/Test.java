f75438373b0b2cdc5cae5fd043c630eae0049dfb
aceElephants <elephants19766@gmail.com>
2025-05-28 17:48:55 +0300

    I learned dcMotor and Servo

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test.java
@@ -2,16 +2,26 @@ package org.firstinspires.ftc.teamcode;
 
 import com.arcrobotics.ftclib.command.CommandOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp
 public class Test extends CommandOpMode {
+
+    DcMotor motor1;
+    Servo ori1;
+
+
     @Override
     public void initialize() {
-
+        motor1 = hardwareMap.get(DcMotor.class, "ori1");
+        ori1 = hardwareMap.get(Servo.class, "ori0");
     }
 
     @Override
     public void run() {
+        motor1.setPower(1);
+        ori1.setPosition(1);
 
     }
 }

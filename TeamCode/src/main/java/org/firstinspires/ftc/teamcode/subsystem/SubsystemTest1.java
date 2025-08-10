7198e29a4ef322ab128510b1957f742fa2cc9b74
aceElephants <elephants19766@gmail.com>
2025-08-10 11:23:25 +0300

    idk

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/SubsystemTest1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/SubsystemTest1.java
@@ -6,10 +6,20 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class SubsystemTest1 extends SubsystemBase {
     private DcMotor motor1;
+    private DcMotor motor2;
+    private DcMotor motor3;
+    private DcMotor motor4;
 
     public SubsystemTest1(HardwareMap hardwareMap){
         motor1 = hardwareMap.get(DcMotor.class,"motor1");
-        motor1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        motor2 = hardwareMap.get(DcMotor.class,"motor2");
+        motor3 = hardwareMap.get(DcMotor.class,"motor3");
+        motor4 = hardwareMap.get(DcMotor.class,"motor4");
+
+        motor1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        motor2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        motor3.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        motor4.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
     }
 
     public void setPower(double power){
